8bde3129f869ee09f842d62dd11340e9089fbc5d
Nico Zevallos <gnastacast@gmail.com>
2017-05-26 18:22:26 -0400

    Added new ros registration file

---
+++ src/ros_registration.cpp
@@ -0,0 +1,79 @@
+#include "ros/ros.h"
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/vtk_lib_io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/pcl_config.h>
+#include <Eigen/Dense>
+#include <iostream>
+// Registration includes
+#include <type_defs.h>
+#include <dual_quaternion_registration.h>
+#include <registration_est_bingham_kf_rgbd.h>
+using namespace Eigen;
+using namespace std;
+
+PointCloud pclToEigen(const pcl::PCLPointCloud2& input)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudPtr (new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromPCLPointCloud2(input, *cloudPtr);
+  MatrixXf mat = cloudPtr->getMatrixXfMap();
+  PointCloud points(3,mat.cols());
+  points << mat.topLeftCorner(3,mat.cols()).cast<long double>();
+  return points;
+}
+
+class RosRegistration
+{
+
+private:
+  PointCloud ptcldFixed;
+
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  RosRegistration(const string &filePath){
+    // Load in STL file into the ptcldFixed variable
+    cout << "Loading stl from " << filePath << endl << endl;
+    pcl::PolygonMesh mesh;
+    if (pcl::io::loadPolygonFileSTL(filePath, mesh) == 0)
+    {
+      PCL_ERROR("Failed to load STL file\n");
+    }
+    ptcldFixed = pclToEigen(mesh.cloud);
+  }
+
+  void cloudCB(const boost::shared_ptr<const sensor_msgs::PointCloud2>& cloud_msg)
+  {
+    // Container for original & filtered data
+    pcl::PCLPointCloud2 pcl_pc2;
+    pcl_conversions::toPCL(*cloud_msg,pcl_pc2);
+    PointCloud ptcldMoving = pclToEigen(pcl_pc2);
+    cout << ptcldMoving.rows() << ", " << ptcldMoving.cols() << endl << endl;
+    // Run the registration function without normals
+    RegistrationResult result;
+    result = registration_est_bingham_kf_rgbd(&ptcldMoving, &ptcldFixed,
+                                              1, 100, 20,
+                                              .00001, .00001, 1);
+  }
+
+};
+
+main (int argc, char** argv)
+{
+  // Initialize ROS
+  ros::init (argc, argv, "dual_quaternion_registration");
+  ros::NodeHandle nh;
+  string filepath = "/home/biomed/registration_ws/src/dvrk_vision/defaults/femur.stl";
+  PointCloud stlPoints;
+  RosRegistration registration(filepath);
+  // Create a ROS subscriber for the input point cloud
+  ros::Subscriber sub = nh.subscribe ("stereo/point_cloud", 1,
+                                      &RosRegistration::cloudCB,
+                                      &registration);
+
+  // Spin
+  ros::spin ();
+}
