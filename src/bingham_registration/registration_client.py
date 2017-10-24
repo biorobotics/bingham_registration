ded20e4c1945dc85afe2ae268839797b24257008
Nico Zevallos <gnastacast@gmail.com>
2017-10-24 15:55:27 -0400

    Renamed a few key files and changed CMake library creation

--- src/bingham_registration/registration_client.py
+++ src/bingham_registration/registration_client.py
@@ -14,15 +14,6 @@ class registration_client:
     def update(self):
         pass;
 
-def add_two_ints_client(x, y):
-    rospy.wait_for_service('stereo/bingham_registration')
-    try:
-        add_two_ints = rospy.ServiceProxy('stereo/bingham_registration', RegistrationService)
-        # resp1 = add_two_ints(x, y)
-        # return resp1.sum
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
-
 def usage():
     return "%s [x y]"%sys.argv[0]
 
@@ -35,7 +26,4 @@ if __name__ == "__main__":
         sys.exit(1)
     print "Requesting %s+%s"%(x, y)
     reg = registration_client();
-    print timeit.timeit('add_two_ints_client(1,2)',
-                        setup="from __main__ import add_two_ints_client",
-                        number=10000)
     # print "%s + %s = %s"%(x, y, add_two_ints_client(x, y))
